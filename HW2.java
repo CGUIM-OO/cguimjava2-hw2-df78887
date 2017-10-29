282b2e5776a6e50109e4ac92ca47875d5f27eb6d
df78887 <32171857+df78887@users.noreply.github.com>
2017-10-29 20:49:25 +0800

    Add files via upload

--- HW2.java
+++ HW2.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 /**
- * @author TODO: please add student ID and name here
+ * @author TODO: B0544123 SHU LI YUN
  * Try to write some comments for your codes (methods, 15 points)
  */
 public class HW2 {
@@ -15,7 +15,6 @@ public class HW2 {
         
 		int nDeck=Integer.parseInt(testn);
 		Deck deck=new Deck(nDeck);
-		//TODO: please check your output, make sure that you print all cards on your screen (10 points)
 		deck.printDeck();
 		
 		if(isAllCardsCorrect(deck.getAllCards(),nDeck)){
@@ -26,8 +25,8 @@ public class HW2 {
 	}
 	/**
 	 * This method is used for checking your result, not a part of your HW2
-	 * @param allCards 所有的牌
-	 * @param nDeck 總共有幾副牌
+	 * @param allCards �������
+	 * @param nDeck 蝮賢��嗾����
 	 * @return
 	 */
 	private static boolean isAllCardsCorrect(ArrayList<Card> allCards,int nDeck){
@@ -63,12 +62,9 @@ public class HW2 {
 	}
 
 }
-/**
- * Description: TODO: please add description here
- */
+
 class Deck{
 	private ArrayList<Card> cards;
-	//TODO: Please implement the constructor (30 points)
 	public Deck(int nDeck){
 		cards=new ArrayList<Card>();
 		//1 Deck have 52 cards, https://en.wikipedia.org/wiki/Poker
@@ -77,21 +73,33 @@ class Deck{
 		//Card card=new Card(1,1); ->means new card as clubs ace
 		//cards.add(card);
 		//Sample code end
-
-	}	
-	//TODO: Please implement the method to print all cards on screen (10 points)
+		for(int d=0;d<nDeck;d++)
+		{
+			for(int i=1;i<5;i++)
+	        {
+	        	for(int j=1;j<14;j++)
+	        	{
+	        		Card card=new Card(i,j);
+	        		cards.add(card);
+	        	}
+	        }
+		}//對於每一個Deck而言，都有4個suit與13個rank。將每一張新生成的card加到cards的ArrayList裡
+	}
 	public void printDeck(){
-		//Hint: print all items in ArrayList<Card> cards, 
-		//TODO: please implement and reuse printCard method in Card class (5 points)
-
+		for(int i=0;i<cards.size();i++)
+		{
+			
+			Card card = new Card (cards.get(i).getSuit(),cards.get(i).getRank());
+			//將每一張card的數值實體化
+			card.printCard();
+			//使用printCard將實體化後的card顯示在螢幕上
+		}
 	}
 	public ArrayList<Card> getAllCards(){
 		return cards;
 	}
 }
-/**
- * Description: TODO: please add description here
- */
+
 class Card{
 	private int suit; //Definition: 1~4, Clubs=1, Diamonds=2, Hearts=3, Spades=4
 	private int rank; //1~13
@@ -102,11 +110,22 @@ class Card{
 	public Card(int s,int r){
 		suit=s;
 		rank=r;
-	}	
-	//TODO: 1. Please implement the printCard method (20 points, 10 for suit, 10 for rank)
+	}
 	public void printCard(){
-		//Hint: print (System.out.println) card as suit,rank, for example: print 1,1 as Clubs Ace
-
+		if(suit==1)
+			System.out.print("Clubs ");
+		else if(suit==2)
+			System.out.print("Diamonds ");
+		else if(suit==3)
+			System.out.print("Hearts ");
+		else
+			System.out.print("Spades ");
+		if(rank==1)
+			System.out.println("Ace");
+		else
+			System.out.println(rank);
+		//如果suit=1就顯示Clubs，如果suit=2就顯示Diamonds，如果suit=3就顯示Hearts，如果suit=4就顯示Spades
+		//如果rank=1就顯示Ace，否則顯示原來的數值
 	}
 	public int getSuit(){
 		return suit;
